ebb760d9f5a17d227b866d75d8d9bd6b7aba21b9
Zeus6768 <akaps6768@gmail.com>
2023-01-23 15:08:28 +0900

    fix: 출시 전에는 테스트용 로컬 profile만 사용

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -4,9 +4,10 @@ ABSPATH=$(readlink -f "$0")
 ABSDIR=$(dirname "$ABSPATH")
 
 source "${ABSDIR}"/base.sh
-source "${ABSDIR}"/profile.sh
+#source "${ABSDIR}"/profile.sh
 
 JAR_DIR="$PROJECT_ROOT/jar"
+#IDLE_PROFILE=$(find_idle_profile)
 
 echo "$TIME_NOW > Copy JAR file to $JAR_DIR" >> "$DEPLOY_LOG"
 cp "$PROJECT_ROOT"/*.jar "$JAR_DIR"/
@@ -17,8 +18,9 @@ chmod +x "$JAR_NAME"
 echo "$TIME_NOW > $JAR_NAME 실행" >> "$DEPLOY_LOG"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,"$PROJECT_ROOT"/application-key.yml \
-  -Dspring.profiles.active="$IDLE_PROFILE" \
   "$JAR_NAME" > "$APP_LOG" 2> "$ERROR_LOG" &
+
+# -Dspring.profiles.active=$IDLE_PROFILE
 
 CURRENT_PID=$(pgrep -f JAR_NAME)
 echo "$TIME_NOW > 실행된 PID $CURRENT_PID" >> "$DEPLOY_LOG"

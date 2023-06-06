9c581ebc9115c7b3b9e0312a24603c0e111b2582
Zeus6768 <akaps6768@gmail.com>
2023-06-06 15:18:20 +0900

    fix: 서버 상태 확인용 api 클래스명 수정

--- src/main/java/org/standard/dreamcalendar/controller/HealthCheckController.java
+++ src/main/java/org/standard/dreamcalendar/controller/HealthCheckController.java
@@ -1,24 +1,22 @@
 package org.standard.dreamcalendar.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
-public class TestController {
+@RestController
+public class HealthCheckController {
 
     @Value("${spring.security.user.password}")
-    private String adminAuth;
+    protected String adminAuth;
 
     @GetMapping("/test")
-    public ResponseEntity<HttpStatus> test(@RequestHeader("Authorization") String authorization) {
-
+    ResponseEntity test(@RequestHeader("Authorization") String authorization) {
         if (!authorization.equals(adminAuth)) {
             return ResponseEntity.badRequest().build();
         }
-
         return ResponseEntity.ok().build();
     }
-
 }

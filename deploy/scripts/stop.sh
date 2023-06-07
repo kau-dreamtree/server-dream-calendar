77d8ae5cc72fee4ab2d684d8b97ed89f6b058ecc
Zeus6768 <akaps6768@gmail.com>
2023-06-08 02:31:15 +0900

    github actions test

--- deploy/scripts/stop.sh
+++ deploy/scripts/stop.sh
@@ -15,13 +15,3 @@ else
   echo "$TIME_NOW $0 > 실행중인 $IDLE_PID 애플리케이션을 종료합니다." >> $DEPLOY_LOG
   kill -15 $IDLE_PID
 fi
-
-#CURRENT_PID=$(lsof -ti tcp:8080)
-#
-#if [ -z "$CURRENT_PID" ]
-#then
-#  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다." >> "$DEPLOY_LOG"
-#else
-#  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션을 종료합니다." >> "$DEPLOY_LOG"
-#  kill -15 "$CURRENT_PID"
-#fi

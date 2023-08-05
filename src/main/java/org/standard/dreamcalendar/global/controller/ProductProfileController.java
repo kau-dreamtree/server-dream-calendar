06d98f9ca44d5c9938cd40f94e71a787f61d8600
Zeus6768 <akaps6768@gmail.com>
2023-08-05 09:36:01 +0900

    사용하지 않는 Slf4j 애너테이션 삭제

--- src/main/java/org/standard/dreamcalendar/global/controller/ProductProfileController.java
+++ src/main/java/org/standard/dreamcalendar/global/controller/ProductProfileController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ProductProfileController {

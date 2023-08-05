db54687db2d4e38d536bdc2d5898d4a66eb3a6fe
Zeus6768 <akaps6768@gmail.com>
2023-08-05 09:27:16 +0900

    사용되지 않는 slf4j 애너테이션 삭제

--- src/main/java/org/standard/dreamcalendar/domain/user/UserController.java
+++ src/main/java/org/standard/dreamcalendar/domain/user/UserController.java
@@ -1,7 +1,6 @@
 package org.standard.dreamcalendar.domain.user;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +11,6 @@ import org.standard.dreamcalendar.domain.user.dto.response.TokenResponse;
 
 import java.security.NoSuchAlgorithmException;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping
